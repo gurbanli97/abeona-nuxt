83a2e651eda14df9bfe789c029c2be5aaab82966
Ali Gurbanli <53146072+gurbanli97@users.noreply.github.com>
2022-04-04 10:05:45 +0400

    Update axios.js

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 export default function ({ $axios, error: nuxtError }) {
     $axios.onRequest(config => {
       config.headers['Content-Type'] = 'application/json';
-      config.headers['Authorization'] = 'Token 59a1e6bf9efe056cc908427a64aa02568ae91c2c'
+      config.headers['Authorization'] = 'Token'
     });
   
     $axios.onError(error => {
